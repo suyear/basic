c09aa235f5a3db8c8dc1036b731e42cd1891a904
suyear <suyear@qq.com>
2017-04-20 22:19:47 +0800

    项目初始化

---
+++ basic-template-parent/src/main/java/cn/enue/basic/App.java
@@ -0,0 +1,13 @@
+package cn.enue.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
